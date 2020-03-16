9f1f25e284ccb394cbdd1dd3d04ca51e46bb06f7
Its-astonishing <grifon187@gmail.com>
2020-03-16 16:18:04 +0300

    [new] New filters

---
+++ Dilation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace CV_lab_1
+{
+    class Dilation : IMorphology
+    {
+        protected override Color getNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            int xCenter = IMorphology.m_x / 2;
+            int yCenter = IMorphology.m_y / 2;
+
+            if (((xCenter <= x) && (sourceImage.Width - xCenter) > x) &&
+                ((yCenter <= y) && (sourceImage.Height - yCenter) > y))
+            {
+                return findMax(sourceImage, x, y);
+            }
+
+            return sourceImage.GetPixel(x, y);
+        }
+    }
+}
---
+++ IMorphology.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace CV_lab_1
+{
+    abstract class IMorphology : IFilter
+    { 
+        protected int getGrayscale(Color color)
+        {
+            return (color.R + color.G + color.B) / 3;
+        }
+
+        protected Color findMax(Bitmap sourceImage, int x, int y)
+        {
+            Color max = Color.FromArgb(0, 0, 0);
+
+            for (int i = x - IMorphology.m_x / 2; i < x + IMorphology.m_x / 2; ++i)
+            {
+                for (int j = y - IMorphology.m_y / 2; j < y + IMorphology.m_y / 2; ++j)
+                {
+                    int currentColor = getGrayscale(sourceImage.GetPixel(i, j));
+
+                    if (currentColor > getGrayscale(max))
+                    {
+                        max = sourceImage.GetPixel(i, j);
+                    }
+                }
+            }
+
+            return max;
+        }
+
+        protected Color findMin(Bitmap sourceImage, int x, int y)
+        {
+            Color min = Color.FromArgb(0xff, 0xff, 0xff);
+
+            for (int i = x - IMorphology.m_x / 2; i < x + IMorphology.m_x / 2; ++i)
+            {
+                for (int j = y - IMorphology.m_y / 2; j < y + IMorphology.m_y / 2; ++j)
+                {
+                    int currentColor = getGrayscale(sourceImage.GetPixel(i, j));
+
+                    if (currentColor < getGrayscale(min))
+                    {
+                        min = sourceImage.GetPixel(i, j);
+                    }
+                }
+            }
+
+            return min;
+        }
+
+        public static int m_x = 10;
+        public static int m_y = 10;
+    }
+}
