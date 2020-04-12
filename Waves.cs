ec4f2642bd27faada3be0d9bc517c74a98d57f19
Its-astonishing <grifon187@gmail.com>
2020-04-12 23:15:55 +0300

    [new] Add waves filter

---
+++ Waves.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace CV_lab_1
+{
+    class Waves1 : IFilter
+    {
+        protected override Color getNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            int new_x = (int)(x + (20 * Math.Sin(2 * Math.PI * y / 60)));
+            int new_y = y;
+
+            if (new_x < 0 || sourceImage.Width <= new_x)
+            {
+                return Color.FromArgb(0, 0, 0);
+            }
+
+            return sourceImage.GetPixel(new_x, new_y);
+        }
+    }
+
+    class Waves2 : IFilter
+    {
+        protected override Color getNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            int new_x = (int)(x + (20 * Math.Sin(2 * Math.PI * y / 30)));
+            int new_y = y;
+
+            if (new_x < 0 || sourceImage.Width <= new_x)
+            {
+                return Color.FromArgb(0, 0, 0);
+            }
+
+            return sourceImage.GetPixel(new_x, new_y);
+        }
+    }
+}
